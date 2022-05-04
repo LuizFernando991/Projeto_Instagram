be0bd62500798a072a35e080ba72f814641c4af2
luiz.oliveira <luiz.oliveira@newm.com.br>
2022-05-04 19:23:45 -0300

    delete and get user stories route

--- backend/src/controllers/StorieController.ts
+++ backend/src/controllers/StorieController.ts
@@ -5,8 +5,8 @@ import getToken from '../helpers/get-token'
 
 
 export default class StorieController {
-    
-    public static async createStorie(req: Request, res: Response) {
+
+    public static async createStorie(req: Request, res: Response) : Promise<Response> {
         const image : any = req.file
         if(!image){
             return res.status(422).json({ message : 'image is required' })
@@ -39,6 +39,49 @@ export default class StorieController {
         }
 
         
+    }
+
+    public static async getUserStories(req: Request, res: Response) : Promise<Response> {
+        const userId : string = req.params.id
+        if(!userId){
+            return res.status(422).json({ message : 'user id is required'})
+        }
+        const userStories = await Storie.find({ postedBy : userId})
+            .populate("postedBy", ["name", "username", "imageProfile"])
+            .populate("visualizedBy", ["name", "username", "imageProfile"])
+        return res.status(200).json({ stories : userStories })
+
+    }
+
+    public static async deleteStorie(req: Request, res: Response) : Promise<Response> {
+        const { storieId } : { storieId : string} = req.body
+        if(!storieId){
+            return res.status(422).json({ message : 'storie id is required' })
+        }
+        //get user
+        const token = getToken(req)
+        if(!token){
+            return res.status(422).json({ message : 'invalid token' })
+        }
+        const user = await getUserByToken(token, res)
+        if(!user){
+            return res.status(422).json({ message : 'invalid token' })
+        }
+        //get storie
+        const post = await Storie.findOne({ _id : storieId })
+        if(!post){
+            return res.status(404).json({ message : 'storie not found' })
+        }
+        //check if user is owner of storie
+        if(user._id.toString() !== post.postedBy.toString()){
+            return res.status(401).json({ message : 'unauthorized' })
+        }
+        try{
+            post.remove()
+            return res.status(200).json({ message : 'storie deleted'})
+        }catch(err) {
+            return res.status(500).json({ message : 'internal error'})
+        }
     }
     
 }
--- backend/src/routes/StorieRoutes.ts
+++ backend/src/routes/StorieRoutes.ts
@@ -8,6 +8,8 @@ import verifyTokenMiddleware from '../middlewares/verifyTokenMiddleware'
 const router = express.Router()
 
 router.post('/createstorie', verifyTokenMiddleware, imageUpload.single('image'), StorieController.createStorie)
+router.get('/user/:id', StorieController.getUserStories)
+router.delete('/deletestorie', verifyTokenMiddleware, StorieController.deleteStorie)
 
 
 export default router
