47b48de970acb2050468a730a85619a02d06bb97
luiz.oliveira <luiz.oliveira@newm.com.br>
2022-08-25 17:23:34 -0300

    protecting edit route

--- frontend/src/pages/edit.tsx
+++ frontend/src/pages/edit.tsx
@@ -1,5 +1,24 @@
+import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
 import { EditPage } from '../pages-templates/Edit'
 
 export default function Edit() {
     return <EditPage />
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    //getAPIClient
+    const { 'instagram-token': token } = parseCookies(ctx)
+    if (!token) {
+        return {
+            redirect: {
+                destination: '/auth/login',
+                permanent: false,
+            },
+        }
+    }
+
+    return {
+        props: {},
+    }
 }
